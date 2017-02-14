964ae02b2a97f4587660764b92623058ffa592f5
courtney <coa.mcallister@gmail.com>
2017-02-13 21:44:32 -0500

    built a basic counter that increases or decreases by one starting at zero. also added a reset button that resets the count to zero

---
+++ public/js/app.js
@@ -0,0 +1,49 @@
+var counter = {
+
+  count: 0,
+
+  init: function(){
+    counter.cacheDom();
+    counter.addEventListeners();
+    counter.render();
+  },
+
+  cacheDom: function(){
+    counter.addButton = document.querySelector('#add');
+    counter.subtractButton = document.querySelector('#subtract');
+    counter.totalCount = document.querySelector('#total-count');
+    counter.resetButton = document.querySelector('#reset');
+  },
+
+  render: function(){
+    console.log(counter.count);
+    var displayCount = `<h1>${counter.count}</h1>`
+    counter.totalCount.innerHTML = displayCount;
+    return displayCount
+  },
+
+
+  addEventListeners: function(){
+    counter.addButton.addEventListener('click', counter.addOne);
+    counter.subtractButton.addEventListener('click', counter.subtractOne);
+    counter.resetButton.addEventListener('click', counter.resetCount);
+  },
+
+  resetCount: function(){
+    counter.count = 0;
+    counter.render();
+  },
+
+  addOne: function(){
+    counter.count += 1;
+    counter.render();
+  },
+
+  subtractOne: function() {
+    counter.count -= 1;
+    counter.render();
+  },
+
+}
+
+counter.init();
